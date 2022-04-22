b7bffdbdc1c5e8f04854fe3c50624aa363857640
Isaac Shafer <izakshafer@gmail.com>
2022-04-22 11:52:19 -0600

    initial commit

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' onChange={e => {setInput(e.target.value)}}/>
+      <p>{input}</p>
     </div>
   );
 }
